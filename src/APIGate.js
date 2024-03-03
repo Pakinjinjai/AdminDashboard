4208732bc102a0c12b533a99780981a6eb339ae7
PAKIN JINJAI <63201161@kmitl.ac.th>
2024-03-03 14:22:06 +0700

    Login update

--- src/APIGate.js
+++ src/APIGate.js
@@ -1,14 +1,10 @@
-//Development 
-const baseURL = 'http://localhost:8080/api';
+///Production///
+// const baseURL = " URL " 
 
-//Production
-// const baseURL = 'http://"URLProduct"/api';
-
-
-//PATH API
-const LOGIN_API = '/user-login'
-
-exports = {
+///Development///
+const baseURL = "http://localhost:8080/api" 
+const LOGIN_API = "/user-login";
+export {
     baseURL,
-    LOGIN_API
+    LOGIN_API,
 }
