bc80e35f17251364a57eddfa91737e39e98cd530
PAKIN JINJAI <63201161@kmitl.ac.th>
2024-03-03 14:03:00 +0700

    Add APIGATE.js

---
+++ src/APIGate.js
@@ -0,0 +1,14 @@
+//Development 
+const baseURL = 'http://localhost:8080/api';
+
+//Production
+// const baseURL = 'http://"URLProduct"/api';
+
+
+//PATH API
+const LOGIN_API = '/user-login'
+
+exports = {
+    baseURL,
+    LOGIN_API
+}
