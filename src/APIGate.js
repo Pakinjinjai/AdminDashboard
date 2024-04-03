45eed911517b55d393cf1601e290893f86918017
PAKIN JINJAI <pakin.281244@gmail.com>
2024-04-04 00:22:32 +0700

    Update APIGate.js

--- src/APIGate.js
+++ src/APIGate.js
@@ -5,8 +5,14 @@
 const baseURL = "http://localhost:8080/api" 
 const LOGIN_API = "/user-login";
 const GETALLUSERS_API = "/all-data-user";
+const EDITUSERS_API = "/upload-image"; //parameter {userId} ต่อหลัง URL ex./upload-image/{userId}
+/** 'Authorization': `Bearer Include accessToken in the header'Content-Type': 'application/json' **/ 
+const UPDATEUSER = '/user-update'
+
 export {
     baseURL,
     LOGIN_API,
-    GETALLUSERS_API
+    GETALLUSERS_API,
+    EDITUSERS_API,
+    UPDATEUSER
 }
